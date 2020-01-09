fd46cee076e5bef2dace91719b22cf922c6b7bd1
Taylor Payne <taylor_payne@byu.edu>
2020-01-09 12:37:58 -0700

    Implement Pokecard Component

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import './Pokecard.css';
 
-function Pokecard() {
-  return (
-    <div className="Pokedex">
-      Pokecard
-    </div>
-  );
+function padDigits(number, digits) {
+  return Array(Math.max(digits - String(number).length + 1, 0)).join(0) + number;
+}
+
+class Pokecard extends React.Component {
+  render() {
+    const { id, name, type, base_experience } = this.props;
+    const pokemonImageUrl = `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${padDigits(id, 3)}.png`
+    return (
+      <div className="Pokecard">
+        <h3 className="Pokecard-h3">{name}</h3>
+        <img src={pokemonImageUrl}></img>
+        <p>Type: {type}</p>
+        <p>EXP: {base_experience}</p>
+      </div>
+    );
+  }
 }
 
 export default Pokecard;
