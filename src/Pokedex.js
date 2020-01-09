4944adcb9b2e330a98199398bb3ca74c5331daab
Taylor Payne <taylor_payne@byu.edu>
2020-01-08 17:43:43 -0700

    Render pokedex string from App

---
+++ src/Pokedex.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './Pokedex.css';
+
+function Pokedex() {
+  return (
+    <div className="Pokedex">
+      Pokedex
+    </div>
+  );
+}
+
+export default Pokedex;
